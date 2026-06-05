db2c4e4ad7aea49d0e9ec589d027e3a92dab8449
cico <nicocidips@gmail.com>
2026-06-05 14:19:32 -0300

    Reemplazar Principal.java por la version corregida de la catedra

--- materiales/tema2/Principal.java
+++ materiales/tema2/Principal.java
@@ -8,11 +8,11 @@ public class Principal {
         input2 = Sistema.read();
 
         C objetoC = new C(input1, input2);
-        
+
+        objetoC.procesarComplejo(input1 + input2, input1 - input2);
+
         int resultado1 = objetoC.obtenerEstado(); 
 
-        int resultado2 = objetoC.procesarComplejo(input1 + input2, input1 - input2);
-
-        Sistema.print(resultado1 + resultado2);
+        Sistema.print(resultado1);
     }
 }
